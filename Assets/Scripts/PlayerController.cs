3036c047bf2d4fd9dc5c7848451238b1211a63d1
GammaTT <52464999+GammaTT@users.noreply.github.com>
2024-10-29 15:10:42 +0900

    Add PlayerController
    플레이어 Move 추가

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    Vector2 curMovementInput;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+
+    Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(curMovementInput.x, rigidbody.velocity.y, curMovementInput.y);
+
+        rigidbody.velocity = direction * moveSpeed;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+}
